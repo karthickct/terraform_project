7740915199650dd14c1913e18b1bf52b56923aca
karthickct <67188240+karthickct@users.noreply.github.com>
2020-06-20 20:32:20 +0800

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -29,26 +29,26 @@ resource "aws_security_group" "sg_devops" {
     ingress {
         from_port = 80
         to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["${var.private_subnet_cidr}"]
+        protocol = "HTTP"
+        cidr_blocks = ["${var.public_subnet_cidr}"]
     }
 
     ingress {
         from_port = 22
         to_port = 22
-        protocol = "tcp"
+        protocol = "ssh"
         cidr_blocks = ["0.0.0.0/0"]
     }
    egress {
         from_port = 80
         to_port = 80
-        protocol = "tcp"
+        protocol = "http"
         cidr_blocks = ["0.0.0.0/0"]
     }
     egress {
         from_port = 22
         to_port = 22
-        protocol = "tcp"
+        protocol = "ssh"
         cidr_blocks = ["${var.vpc_cidr}"]
     }
 
